e974d9262246cf03883251063938465cf47a83cd
rockmel03 <kamalswag2@gmail.com>
2024-03-29 02:14:50 +0530

    configure authentication and database services

---
+++ src/appwrite/Config.jsx
@@ -0,0 +1,65 @@
+// create services like
+// create, deleate , update , getsingle, getmultiple, query
+
+import { Client, Databases, ID, Storage } from "appwrite";
+import conf from "../conf/conf";
+
+export class Service {
+  client = new Client();
+  databases;
+  bucket;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+  }
+
+  async createProduct({
+    id,
+    image,
+    title,
+    description,
+    price,
+    collection,
+    seller,
+  }) {
+    try {
+      return await this.databases.createDocument(
+        conf.appwriteProductsDatabaseId,
+        conf.appwriteProductsCollectionId,
+        id,
+        { image, title, description, price, collection, seller }
+      );
+    } catch (error) {
+      console.log("Services:: createProduct :: Error", error);
+    }
+  }
+
+  async updateProduct({ id, image, title, description, price, collection }) {
+    try {
+      return await this.databases.updateDocument(
+        conf.appwriteProductsDatabaseId,
+        conf.appwriteProductsCollectionId,
+        id,
+        { image, title, description, price, collection } //[TODO]:status bhi add krna hai
+      );
+    } catch (error) {
+      console.log("Services:: updateProduct :: Error", error);
+    }
+  }
+
+  async deleteProduct(slug) {
+    try {
+      return await this.databases.deleteDocument(
+        conf.appwriteProductsDatabaseId,
+        conf.appwriteProductsCollectionId,
+        slug
+      );
+    } catch (error) {
+      console.log("Services:: deleteProduct :: Error", error);
+    }
+  }
+}
